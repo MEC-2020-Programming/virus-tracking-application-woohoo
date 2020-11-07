0a344cc323af4f92f40c69d3af913f309d2cd746
Isabelle Lai <49911729+isa-Lai@users.noreply.github.com>
2020-11-08 02:34:10 +0800

    Update data.py

--- data.py
+++ data.py
@@ -1,4 +1,5 @@
 
+
 #read file
 infile = open('testing.txt','r')
 content = infile.read()
@@ -6,11 +7,10 @@ data = content.split('\n')
 country_number = 0
 for i in data:
     data[country_number] = i.split(',')
-    for k in len(data[country_number]):
-        if k=1:
-            data[country_number][1]  = data[country_number][1][2:-3]
-        else:
-            data[country_number][k]  = int(data[country_number][k][2:-2])    
+    k=0
+    for ele in data[country_number]:
+        data[country_number][k]= ele.strip(' []"')
+        k+=1
     country_number+=1
 print (data) 
 infile.close()
